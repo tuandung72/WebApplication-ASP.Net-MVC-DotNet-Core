6b15648d85c027d2b4af53a0d979c1861e7c6ffd
tuandung <nguyentuandung725@gmail.com>
2024-01-12 23:09:12 +0700

    tuandung

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net_MVC_TH_01.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            var studentModel = new StudentModel
+            {
+                FullName = "Nguyen Van A",
+                StudentID = "123456",
+                Address = "Hanoi, Vietnam",
+                Email = "nguyenvana@example.com",
+                Hobbies = "Reading, Coding"
+            };
+
+            return View(studentModel);
+        }
+    }
+}
---
+++ Models/Model StudentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP.Net_MVC_TH_01.Models
+{
+    public class Model_StudentModel
+    {
+            public string FullName { get; set; }
+            public string StudentID { get; set; }
+            public string Address { get; set; }
+            public string Email { get; set; }
+            public string Hobbies { get; set; }
+        }
+    }
+
